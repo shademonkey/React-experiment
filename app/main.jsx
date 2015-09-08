a7b093badf0a2fc009c594a410ebacf00c7312d8
Andrew M. Clark <shademonkey013@gmail.com>
2015-09-08 11:55:51 -0600

    first commit

---
+++ app/main.jsx
@@ -0,0 +1,18 @@
+console.log('Hello from JSX!');
+
+var GroceryItemList = require('./components/GroceryItemList.jsx');
+var React = require('react/addons');
+var groceryItemStore = require('./stores/GroceryItemStore.jsx');
+
+var initial = groceryItemStore.getItems();
+
+function render() {
+  React.render(<GroceryItemList items={ initial } />, app);
+}
+
+groceryItemStore.onChange(function (items){
+  initial = items;
+  render();
+})
+
+render();
